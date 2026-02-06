6278c6c2f935439760bd63d0eff23a51e727c0fe
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-06 17:56:58 -0300

    Update Ex2p16.java

--- Ex2p16/Ex2p16.java
+++ Ex2p16/Ex2p16.java
@@ -10,6 +10,23 @@ import java.util.Scanner;
 
 public class Ex2p16{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in)
+			
+		int x, y;
+			
+		System.out.println("Digite o primeiro número: ");
+		x = sc.nextInt();
+		
+		System.out.println("Digite o segundo número: ");
+		y = sc.nextInt();
+		
+		if(x > y){
+			System.out.println(x +" is larger");
+		}else if(y > x){
+			System.out.println(y +" is larger");
+		}else{
+			System.out.println("These number are esqual");
+		}
 		
 	}
 }
