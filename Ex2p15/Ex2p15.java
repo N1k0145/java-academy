ce1922e0aa3f77c154f389201d81ccc42aab0c4a
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-06 17:19:25 -0300

    Create Ex2p15.java

---
+++ Ex2p15/Ex2p15.java
@@ -0,0 +1,15 @@
+/*
+	2.15
+	Escreva um aplicativo que solicita ao usuário inserir dois inteiros,
+	obtém do usuário esses números e imprime sua soma, produto, diferença
+	e quociente(divisão).
+*/
+
+import java.util.Scanner;
+
+public class Ex2p15{
+	public static void main(String[] args){
+		
+	}
+		
+}
