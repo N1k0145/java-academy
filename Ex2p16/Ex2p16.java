00932470c0be374fe89774103049848570282074
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-06 17:49:47 -0300

    Create Ex2p16.java

---
+++ Ex2p16/Ex2p16.java
@@ -0,0 +1,15 @@
+/*
+	2.16
+	Escreva um aplicativo  que solicita ao usuário inserir dois
+	inteiros, obtém do uauário esses números e exibe o número maior
+	seguido pelas palavras "is larger". Se os números forem iguais,
+	impruima a mensagem "These number are equal"
+*/
+
+import java.util.Scanner;
+
+public class Ex2p16{
+	public static void main(String[] args){
+		
+	}
+}
