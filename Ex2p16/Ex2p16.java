7224a4cb7dd9540a8f268ba0d006b9a662ef04c4
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-06 17:59:49 -0300

    Update Ex2p16.java

--- Ex2p16/Ex2p16.java
+++ Ex2p16/Ex2p16.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Ex2p16{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in)
+		Scanner sc = new Scanner(System.in);
 			
 		int x, y;
 			
