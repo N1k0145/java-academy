83cf381b96c868530161e5356e5d546ef7c54c9d
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-06 17:31:03 -0300

    Update Ex2p15.java

--- Ex2p15/Ex2p15.java
+++ Ex2p15/Ex2p15.java
@@ -9,6 +9,25 @@ import java.util.Scanner;
 
 public class Ex2p15{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y, soma, produto, diferenca, quociente;
+		
+		System.out.println("Digite o primeiro número: ");
+		x = sc.nextInt();
+		
+		System.out.println("Digite o segundo número: ");
+		y = sc.nextInt();
+		
+		soma = (x + y);
+		produto = (x * y);
+		diferenca = (x - y);
+		quociente = (x / y);
+		
+		System.out.println("SOMA = " +soma);
+		System.out.println("PRODUTO = " +produto);
+		System.out.println("DIFERENÇA = " +diferenca);
+		System.out.println("QUOCIENTE = " +quociente);
 		
 	}
 		
