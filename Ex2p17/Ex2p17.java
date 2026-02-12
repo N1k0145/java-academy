ceb5b318b16e6dec11c1305c37dc658627016054
Nikolas <nikolas.heiderich@etec.sp.gov.br>
2026-02-12 10:09:13 -0300

    Update Ex2p17.java

--- Ex2p17/Ex2p17.java
+++ Ex2p17/Ex2p17.java
@@ -4,3 +4,31 @@
 	usuario e exibe a soma, media, produto e os numeros menores e
 	maiores.
 */
+
+import java.util.Scanner;
+
+public class Ex2p17{
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, soma, produto, media;
+		
+		System.out.println("Digite o primeiro numero");
+		a = sc.nextInt();
+		
+		System.out.println("Digite o segundo numero");
+		b = sc.nextInt();
+		
+		System.out.println("Digite o terceiro numero");
+		c = sc.nextInt();
+		
+		soma = a + b + c;
+		
+		produto = a * b * c;
+		
+		media = (a + b + c) / 3;
+		
+		if (){
+		}
+	}
+}
